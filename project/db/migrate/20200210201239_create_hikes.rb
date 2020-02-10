b87dd05a7bc7e66c8e1365f0dfb13c6e043a1759
Alyssa Clarke <alyssa.clarke00@gmail.com>
2020-02-10 15:14:54 -0500

    meow

---
+++ project/db/migrate/20200210201239_create_hikes.rb
@@ -0,0 +1,12 @@
+class CreateHikes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :hikes do |t|
+      t.string :location
+      t.integer :distance
+      t.integer :elevation
+      t.string :difficulty
+
+      t.timestamps
+    end
+  end
+end
