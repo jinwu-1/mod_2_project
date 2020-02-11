d7cc267bb163fe8436f1d7f34ca838a7600ceb0a
Alyssa Clarke <alyssa.clarke00@gmail.com>
2020-02-11 13:55:01 -0500

    new stuff

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = find_me
+    end
+
+
+
+
+    private
+
+    def find_me
+        User.find(params[:id])
+    end
+
 end
