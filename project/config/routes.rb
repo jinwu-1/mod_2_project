866e73362353fa16fbbb29b7a5138d1832a45803
Jin Wu <jinn.wuu@gmail.com>
2020-02-11 13:49:20 -0500

    create review form

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, only: [:show, :new, :create, :edit, :update]
   resources :hikes, only: [:index, :show]
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- project/db/migrate/20200210212355_create_reviews.rb
+++ project/db/migrate/20200210212355_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.integer :rating
+      t.string :description
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :hike, null: false, foreign_key: true
 
