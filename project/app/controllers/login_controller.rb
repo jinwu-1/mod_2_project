68d2d6ffc253d8b427954e1e6f17c23796dd6411
Jin Wu <jinn.wuu@gmail.com>
2020-02-12 14:39:52 -0500

    login broken

--- project/app/controllers/login_controller.rb
+++ project/app/controllers/login_controller.rb
@@ -4,6 +4,13 @@ class LoginController < ApplicationController
     end
 
     def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = [ "User doesn't exist or invalid password" ]
+            redirect_to new_login_path
+        end
     end
     
 end
