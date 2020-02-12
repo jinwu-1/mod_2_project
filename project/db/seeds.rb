5387f5e1a0a0deb8ef40509a6212e2f5f8252cd7
Jin Wu <jinn.wuu@gmail.com>
2020-02-12 15:17:37 -0500

    ughh

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -12,21 +12,21 @@ Hike.destroy_all
 age = (18..99).to_a
 
 
-User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "user1", password: "asdf")
-User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "user2", password: "asdf")
-User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "user3", password: "asdf")
-User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "user4", password: "asdf")
-User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "user5", password: "asdf")
+User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "asdf", password: "asdf")
+User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "qwerty", password: "asdf")
+User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "abcde", password: "asdf")
+User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "hello", password: "asdf")
+User.create(name: Faker::Name.name, age: age.sample, bio: Faker::Hipster.sentence, username: "zzzzz", password: "asdf")
 
 Hike.create(name: "Greenport Trail", distance: 5.4, elevation: 500, difficulty: "Hard")
-Hike.create(name: "Cold Spring Park", distance: 10.2, elevation: 50 , difficulty: "Moderate")
+Hike.create(name: "Cold Spring Park", distance: 7.2, elevation: 50 , difficulty: "Moderate")
 Hike.create(name: "Kingston Trail", distance: 3.5 , elevation: 50, difficulty: "Easy")
-Hike.create(name: "Hudson Valley Park", distance: 7.2, elevation: 124 , difficulty: "Moderate")
-Hike.create(name: "Catskill Hike", distance: 12, elevation: 1000, difficulty: "Hell")
+Hike.create(name: "Hudson Valley Park", distance: 5.2, elevation: 124 , difficulty: "Moderate")
+Hike.create(name: "Catskill Hike", distance: 57, elevation: 5000, difficulty: "Hell")
 
 rating = (1..5).to_a
-5.times do
-    Review.create(description: Faker::Hipster.sentence, rating: rating.sample, user_id: User.all.sample.id, hike_id: Hike.all.sample.id)
+10.times do
+    Review.create(description: Faker::Hipster.paragraph(sentence_count: 3), rating: rating.sample, user_id: User.all.sample.id, hike_id: Hike.all.sample.id)
 end
 
 puts "All done!"
